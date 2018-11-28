f2989b2493a47ee1d5c0df91c0248eabc6a21690
崔海峰 <hf.cui@Ctrip.com>
2018-11-28 19:32:04 +0800

    feat(.postcssrc.js,nerwork.js): 增加兼容性浏览器配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,7 +2,9 @@ module.exports =  {
   "plugins": [
     require("postcss-import")({}),
     require("postcss-url")({}),
-    require("autoprefixer")({}),
+    require("autoprefixer")({
+      browsers: ['ios>=8.0', 'Android>=4.3']
+    }),
     require("./build/postcss-px-to-viewport-mod")({
       viewportWidth: 750,
       viewportHeight: 1334,
--- components/tools/network.js
+++ components/tools/network.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 import { prodApiAddress, devApiAddress } from 'components/tools/constants/index.js'
 import { apiLoading } from './loading/index.js'
-import { jsonp } from './service/jsonp.js'
 
 //? 接口地址
 const API_ROOT = /^(http|https).*ct.ctrip.com/.test(window.location.href) ? prodApiAddress : devApiAddress
 var instance = axios.create({
-  baseURL: `m/`,
+  baseURL: process.NODE_ENV === 'production' ? `${API_ROOT}/m/` : `m/`,
   withCredentials: true,
   timeout: 10000
 })
@@ -25,5 +24,4 @@ axios.interceptors.response.use(res => {
 
 export async function get (url, data = {}) {
   instance.get(url, { params: data })
-  jsonp(`${API_ROOT}/${url}`, data)
 }
